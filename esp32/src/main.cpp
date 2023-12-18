82a3bec6b8eef2bd6f528e8d87f82cfea69e47f6
homin1209 <homin1209@gmail.com>
2023-12-18 11:36:42 +0900

    Arduino 코드 추가

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,149 +1,273 @@
-#include <WiFi.h>
-#include <HTTPClient.h>
+#include <REG.h>
+#include <wit_c_sdk.h>
 #include <Arduino.h>
-#include "esp_wpa2.h"
+#include <M5StickC.h>
+#include <WiFi.h>
 #include <PubSubClient.h>
-#include <Wire.h>
+#include <string>
 
+using namespace std;
 
-const char* ssid = "heensan";
-const char* password = "whitemountain";
+#define RX 32
+#define TX 33
+#define ACC_UPDATE		0x01
+#define GYRO_UPDATE		0x02
+#define ANGLE_UPDATE	0x04
+#define MAG_UPDATE		0x08
+#define READ_UPDATE		0x80
 
-const char* serverAddress = "http://59.14.135.171:21330/update_number/";
-const char* mqtt_server = "192.168.0.16";
+static volatile char s_cDataUpdate = 0, s_cCmd = 0xff; 
 
-const int xPin = 0;
-const int yPin = 2;
-const int zPin = 14;
+const char* ssid = "1309";
+const char* password = "00000000";
+const char* mqtt_server = "192.168.155.230";
 
-int minVal = 265;
-int maxVal = 402;
-
-double x;
-double y;
-double z;
-
-void sendNumberToServer(int number);
-void callback(char* topic, byte* payload, unsigned int length);
-void reconnect();
+const char* location = "2Campus";
+const char* subLocation = "MazeRunner";
+const char* part = "DA";
+const char* sensorType = "Vibration";
+int sensorIndex = 1;
+const char* valueType[3] = {"X", "Y", "Z"};
+const char* value;
+char ch[3][10];
+const char* check = "1";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
-unsigned long lastMsg = 0;
-#define MSG_BUFFER_SIZE	(50)
-// char msg[MSG_BUFFER_SIZE];
-int value = 0;
+
+void CopeCmdData(unsigned char ucData);
+static void CmdProcess(void);
+static void AutoScanSensor(void);
+static void SensorUartSend(uint8_t *p_data, uint32_t uiSize);
+static void SensorDataUpdata(uint32_t uiReg, uint32_t uiRegNum);
+static void Delayms(uint16_t ucMs);
+void setup_wifi();
+void callback(const char* topic, byte* payload, unsigned int length);
+void reconnect();
+int connect_first();
+string str(const char* rc);
+const uint32_t c_uiBaud[8] = {1200, 4800, 9600, 19200, 38400, 57600, 115200, 230400};
 
 void setup() {
-  Serial.begin(9600);
+  	Serial.begin(115200);
+  	Serial2.begin(115200, SERIAL_8N1, RX, TX);
+	setup_wifi();
+	client.setServer(mqtt_server, 1883);
+	client.setCallback(callback);
+	M5.begin();
+    M5.Lcd.setRotation(3);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setTextSize(1);
+    M5.Lcd.setCursor(5, 5);
+    M5.Lcd.println("        X     Y     Z");
+	WitInit(WIT_PROTOCOL_NORMAL, 0x50);
+	WitSerialWriteRegister(SensorUartSend);
+	WitRegisterCallBack(SensorDataUpdata);
+  	WitDelayMsRegister(Delayms);
+	WitSetOutputRate(RRATE_200HZ);
+	AutoScanSensor();
+	if (connect_first() == 0){
+		exit(1);
+	}
+}
+int i;
+float fAcc[3], fGyro[3], fAngle[3];
+void loop() {
+	if (!client.connected()) {
+		reconnect();
+	}
+	client.loop();
+  	delay(1000);
+	while (Serial2.available())
+	{
+		WitSerialDataIn(Serial2.read());
+	}
+	while (Serial.available()) 
+	{
+		CopeCmdData(Serial.read());
+	}
+	if(s_cDataUpdate)
+	{
+		for(i = 0; i < 3; i++)
+		{
+			fAcc[i] = sReg[AX+i] / 32768.0f * 16.0f;
+		}
+		if(s_cDataUpdate & ACC_UPDATE)
+		{
+			M5.Lcd.setCursor(5, 20);
+			M5.Lcd.printf("Acc  :%.2f  %.2f  %.2f", fAcc[0], fAcc[1], fAcc[2]);
+			for(int i = 0; i < 3; i++){
+				sprintf(ch[i], "%f", fAcc[i]);
+				printf("%s: %s\n", valueType[i], ch[i]);
+				value = str(valueType[i]).c_str();
+				client.publish(value, ch[i]);
+			}
+			s_cDataUpdate &= ~ACC_UPDATE;
+		}
+		s_cDataUpdate = 0;
+	}
+}
+void CopeCmdData(unsigned char ucData)
+{
+	static unsigned char s_ucData[50], s_ucRxCnt = 0;
+	
+	s_ucData[s_ucRxCnt++] = ucData;
+	if(s_ucRxCnt<3) return;
+	if(s_ucRxCnt >= 50) s_ucRxCnt = 0;
+	if(s_ucRxCnt >= 3)
+	{
+		if((s_ucData[1] == '\r') && (s_ucData[2] == '\n'))
+		{
+			s_cCmd = s_ucData[0];
+			memset(s_ucData,0,50);
+			s_ucRxCnt = 0;
+		}
+		else 
+		{
+			s_ucData[0] = s_ucData[1];
+			s_ucData[1] = s_ucData[2];
+			s_ucRxCnt = 2;
+			
+		}
+	}
+}
+static void SensorUartSend(uint8_t *p_data, uint32_t uiSize)
+{
+  Serial2.write(p_data, uiSize);
+  Serial2.flush();
+}
+static void Delayms(uint16_t ucMs)
+{
+  delay(ucMs);
+}
+static void SensorDataUpdata(uint32_t uiReg, uint32_t uiRegNum)
+{
+	int i;
+    for(i = 0; i < uiRegNum; i++)
+    {
+        switch(uiReg)
+        {
+            case AZ:
+				s_cDataUpdate |= ACC_UPDATE;
+            break;
+            case GZ:
+				s_cDataUpdate |= GYRO_UPDATE;
+            break;
+            case HZ:
+				s_cDataUpdate |= MAG_UPDATE;
+            break;
+            case Yaw:
+				s_cDataUpdate |= ANGLE_UPDATE;
+            break;
+            default:
+				s_cDataUpdate |= READ_UPDATE;
+			break;
+        }
+		uiReg++;
+    }
+}
+
+static void AutoScanSensor(void)
+{
+	int i, iRetry;
+	
+	for(i = 0; i < sizeof(c_uiBaud)/sizeof(c_uiBaud[0]); i++)
+	{
+		Serial2.begin(c_uiBaud[i]);
+    	Serial2.flush();
+		iRetry = 2;
+		s_cDataUpdate = 0;
+		do
+		{
+			WitReadReg(AX, 3);
+			delay(200);
+      while (Serial2.available())
+      {
+        WitSerialDataIn(Serial2.read());
+      }
+			if(s_cDataUpdate != 0)
+			{
+				return ;
+			}
+			iRetry--;
+		}while(iRetry);		
+	}
+}
+void setup_wifi() {
+
   delay(10);
+
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
 
   WiFi.begin(ssid, password);
 
   while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("WiFi 연결 중...");
+    delay(500);
+    Serial.print(".");
   }
 
-  Serial.println("WiFi 연결 성공");
+  randomSeed(micros());
 
-  // client.setServer(mqtt_server, 1883);
-  // client.setCallback(callback);
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // if (!client.connected()) {
-  //   reconnect();
-  // }
-  // client.loop();
-
-  // int randomNumber = random(0, 100);  // 0부터 99까지의 랜덤한 숫자 생성
-  // Serial.println("랜덤 숫자: " + String(randomNumber));
-  // sendNumberToServer(randomNumber);
-  // delay(1000);  // 5초마다 숫자를 서버로 보냄
-
-  int xRead = analogRead(xPin);
-  int yRead = analogRead(yPin);
-  int zRead = analogRead(zPin);
-
-  int xAng = map(xRead, minVal, maxVal, -90, 90);
-  int yAng = map(yRead, minVal, maxVal, -90, 90);
-  int zAng = map(zRead, minVal, maxVal, -90, 90);
-
-  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
-  y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
-  z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
-
-  Serial.print("x: ");
-  Serial.print(x);
-  Serial.print(" | y: ");
-  Serial.print(y);
-  Serial.print(" | z: ");
-  Serial.println(z);
-  delay(100);
-
-  // unsigned long now = millis();
-  // if (now - lastMsg > 2000) {
-  //   lastMsg = now;
-  //   ++value;
-  //   char* msg = (char *)randomNumber;
-  //   snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-  //   Serial.print("Publish message: ");
-  //   Serial.println(msg);
-  //   client.publish("outTopic", msg);
-  // }
-}
-
-void sendNumberToServer(int number) {
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    // 서버 주소로 POST 요청 보냄
-    http.begin(serverAddress);
-    http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-    String postData = "number=" + String(number);  // 서버로 보낼 데이터 생성
-
-    int httpResponseCode = http.POST(postData);
-
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println("서버 응답: " + response);
-    } else {
-      Serial.println("오류 발생. 오류 코드: " + String(httpResponseCode));
-    }
-
-    http.end();
-  } else {
-    Serial.println("WiFi 연결이 끊겼습니다.");
-  }
-}
-
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(const char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+  client.unsubscribe(str("Confirm").c_str());
 }
 
 void reconnect() {
-  // Loop until we're reconnected
   while (!client.connected()) {
-    Serial.print("Attempting MQTT connection...");
-    // Create a random client ID
-    String clientId = "ESP8266Client-";
-    clientId += String(random(0xffff), HEX);
-    // Attempt to connect
+    String clientId = "1111";
     if (client.connect(clientId.c_str())) {
-      Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
-    } else {
-      Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+		value = str("Register").c_str();
+		client.publish(value, check);
+    } 
+	else {
+		Serial.println("1분 대기");
+		delay(60000);
     }
   }
+}
+
+int connect_first() {
+	int result = 0;
+	String clientId = "1";
+	if (client.connect(clientId.c_str())) {
+		value = str("Confirm").c_str();
+		client.subscribe(value, 1);
+		
+		value = str("Register").c_str();
+		client.publish(value, check);
+
+		result = 1;
+	}
+	return result;
+}
+
+string str(const char* rc){
+	string rcstr = "ICCMS/";
+	rcstr += location;
+	rcstr += "/";
+	rcstr += subLocation;
+	rcstr += "/";
+	rcstr += part;
+	rcstr += "/";
+	rcstr += sensorType;
+	rcstr += "/";
+	rcstr += String(sensorIndex).c_str();
+	rcstr += "/";
+	rcstr += rc;
+	return rcstr;
 }
